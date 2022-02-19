1a49e0e008d08fa109f3908defa31477233c1a9e
Jean Pierre <jeanjara.15926@gmail.com>
2022-02-18 20:47:10 -0500

    18/02.3

--- dal/exportar.js
+++ dal/exportar.js
@@ -6,29 +6,8 @@ let pool = cnx.pool;
 const exportar= (request, response)=> {
     var obj = valida.validaToken(request)
     if(obj.estado){
-
-        let cadena = 'select pe.n_idpl_estructura, ea.n_idpl_estructuraarmado, pl.c_nombre, pe.c_nro_se, pe.c_circuito, pe.c_codigonodo, pa.c_codigo as codigoap, pa.c_codigo as c_codigoas, \n\r' +
-                ' pa.c_codigo as c_codigoarsse, pe.c_progresiva, pe.c_cota, pe.c_vertice, pe.c_angulo, pe.c_tipoterreno, ea.n_cantidad as n_cantidadea, \n\r'+
-                ' pe.c_etiquetaestructura, pe.c_longitud, pe.c_latitud, pst.c_etiquetacorto, psta.n_cantidad as n_cantidadpsta, ea.n_orientacion, \n\r'+
-                ' pe.n_fases, pe.c_funcion \n\r'+
-                ' from pl_estructura pe \n\r'+
-                ' inner join pl_linea pl on pe.n_idpl_linea = pl.n_idpl_linea and pl.n_borrado = 0\n\r'+
-                ' inner join pl_estructuraarmado ea on pe.n_idpl_estructura = ea.n_idpl_estructura and ea.n_borrado = 0 \n\r'+
-                ' inner join pl_armado pa on ea.n_idpl_armado = pa.n_idpl_armado and pa.n_borrado = 0 \n\r'+
-                ' inner join pl_subtramoarmado psta on pa.n_idpl_armado = psta.n_idpl_armado and psta.n_borrado = 0 \n\r'+
-                ' inner join pl_subtramo pst on psta.n_idpl_subtramo = pst.n_idpl_subtramo and pst.n_borrado = 0 \n\r'+
-                ' inner join pl_zona zn on pl.n_idpl_zona = zn.n_idpl_zona and zn.n_borrado = 0 \n\r'+
-                ' where pe.n_borrado = 0 and pa.n_idpro_proyecto = $1 and zn.n_idpro_proyecto = $1 \n\r'+
-                ' order by pl.c_nombre asc \n\r';
-
-        pool.query( cadena,[request.body.n_idpro_proyecto], 
-            (error, results)=>{
-            if(error){
-                console.log(error)
-                response.status(200).json({ estado: false, mensaje: "DB: error al traer los datos!.", data: null })   
-            }else{
-                var datos = results.rows;                
-                let cadena = ' with poste as ( \n\r'+
+        console.log(request.body.n_idpro_proyecto, request.body.idversion, request.body.idzona, request.body.idtipolinea);
+        let cadena = ' with poste as ( \n\r'+
                     'Select p.n_idpl_estructura, a.c_codigo, ea.n_cantidad from pl_armado a \n\r'+
                     'inner join pl_estructuraarmado ea on a.n_idpl_armado = ea.n_idpl_armado \n\r'+
                     'inner join vw_planos p on ea.n_idpl_estructura = p.n_idpl_estructura          \n\r'+           
@@ -173,13 +152,32 @@ const exportar= (request, response)=> {
                     'inner join vw_planos p on ea.n_idpl_estructura = p.n_idpl_estructura  \n\r'+
                     'where a.c_codigo = \''+'AP_MT'+'\' and a.n_borrado = 0 and a.n_idpro_proyecto = $1 \n\r'+
                     'group by p.n_idpl_estructura, a.c_codigo \n\r'+
-                ') \n\r'+
+                '), \n\r'+
+                'linea as ( \n\r'+
+                    'select pe.n_idpl_estructura, pl.c_nombre, pe.c_nro_se, pe.c_circuito, pe.c_codigonodo, pa.c_codigo as codigoap, pa.c_codigo as c_codigoas,  \n\r'+
+                    'pa.c_codigo as c_codigoarsse, pe.c_progresiva, pe.c_cota, pe.c_vertice, pe.c_angulo, pe.c_tipoterreno,  \n\r'+
+                    'pe.c_longitud, pe.c_latitud, pst.c_etiquetacorto, psta.n_cantidad as n_cantidadpsta, ea.n_orientacion,  \n\r'+
+                    'pe.n_fases, pe.c_funcion  \n\r'+
+                    'from pl_estructura pe  \n\r'+
+                        'inner join pl_linea pl on pe.n_idpl_linea = pl.n_idpl_linea and pl.n_borrado = 0  \n\r'+
+                        'inner join pl_tipolinea tp on pl.n_idpl_tipolinea = tp.n_idpl_tipolinea and tp.n_borrado = 0 \n\r'+
+                        'inner join pl_estructuraarmado ea on pe.n_idpl_estructura = ea.n_idpl_estructura and ea.n_borrado = 0  \n\r'+
+                        'inner join pl_armado pa on ea.n_idpl_armado = pa.n_idpl_armado and pa.n_borrado = 0  \n\r'+
+                        'inner join pl_subtramoarmado psta on pa.n_idpl_armado = psta.n_idpl_armado and psta.n_borrado = 0   \n\r'+
+                        'inner join pl_subtramo pst on psta.n_idpl_subtramo = pst.n_idpl_subtramo and pst.n_borrado = 0  \n\r'+
+                        'inner join pl_zona zn on pl.n_idpl_zona = zn.n_idpl_zona and zn.n_borrado = 0   \n\r'+
+                        'where pe.n_borrado = 0 and pa.n_idpro_proyecto = $1 and zn.n_idpro_proyecto = $1 and pe.n_version = $2 and zn.n_idpl_zona = $3 and tp.n_idpl_tipolinea = $4 \n\r'+
+                        'order by pl.c_nombre asc  \n\r'+
+                ')  \n\r'+
                 'Select p.n_idpl_estructura, a35.n_cantidad as amortiguador_35, a37.n_cantidad as amortiguador_37,  \n\r'+
                     'ri_a.n_cantidad as RI_A, rv_a.n_cantidad as RV_A, RI_MT.n_cantidad as RI_MT, RV_MT.n_cantidad as RV_MT, \n\r'+
                     'RI.n_cantidad as RI, RV.n_cantidad as RV, RIY.n_cantidad as RIY, RVY.n_cantidad as RVY, PAT_1.n_cantidad as PAT_1, \n\r'+
                     'PAT_1C.n_cantidad as PAT_1C, PAT_2.n_cantidad as PAT_2, PAT_3.n_cantidad as PAT_3, PAT_1CS.n_cantidad as PAT_1CS, \n\r'+
                     'PAT_2S.n_cantidad as PAT_2S, PAT_3S.n_cantidad as PAT_3S, PAT_1S.n_cantidad as PAT_1S, AP_BT.n_cantidad as AP_BT, \n\r'+
                     'AP_MT.n_cantidad as AP_MT, \n\r'+
+                    'l.c_nombre, l.c_nro_se, l.c_circuito, l.c_codigonodo, l.codigoap, l.c_codigoas, \n\r'+
+                    'l.c_codigoarsse, l.c_progresiva, l.c_cota, l.c_vertice, l.c_angulo, l.c_tipoterreno, \n\r'+
+                    'l.c_longitud, l.c_latitud, l.c_etiquetacorto, l.n_cantidadpsta, l.n_orientacion, l.n_fases, l.c_funcion, \n\r'+
                     'p.n_cantidad ,p.c_codigo from vw_planos e \n\r'+
                 'left outer join poste p on e.n_idpl_estructura = p.n_idpl_estructura \n\r'+
                 'left outer join ri_a  on e.n_idpl_estructura = ri_a.n_idpl_estructura \n\r'+
@@ -201,20 +199,17 @@ const exportar= (request, response)=> {
                 'left outer join PAT_3S  on e.n_idpl_estructura = PAT_3S.n_idpl_estructura  \n\r'+
                 'left outer join PAT_1S  on e.n_idpl_estructura = PAT_1S.n_idpl_estructura \n\r'+
                 'left outer join AP_BT  on e.n_idpl_estructura = AP_BT.n_idpl_estructura \n\r'+
-                'left outer join AP_MT  on e.n_idpl_estructura = AP_MT.n_idpl_estructura \n\r';
-                pool.query(cadena,[request.body.n_idpro_proyecto],(error, results)=>{
+                'left outer join AP_MT  on e.n_idpl_estructura = AP_MT.n_idpl_estructura \n\r'+
+                'inner join linea l on e.n_idpl_estructura = l.n_idpl_estructura \n\r';
+                pool.query(cadena,[request.body.n_idpro_proyecto, request.body.idversion, request.body.idzona, request.body.idtipolinea],(error, results)=>{
                     if(error){
                         console.log(error)
                         response.status(200).json({ estado: false, mensaje: "DB: error al traer los datos2!.", data: null })   
                     }else{
-                        var datos2 = results.rows;                        
-                        response.status(200).json({ estado: true, mensaje: "", data: {datos: datos, datos2: datos2} });
+                        var datos = results.rows;                        
+                        response.status(200).json({ estado: true, mensaje: "", data:  datos });
                     }
                 })
-                
-            }
-
-        })
     }else{
         response.status(200).json(obj)
     }
