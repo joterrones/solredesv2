1a0d2d460e744385cac0c9f87829c0ab4e5d6669
Jean Pierre <jeanjara.15926@gmail.com>
2022-05-19 12:46:37 -0500

    19/05

--- dal/importacion.js
+++ dal/importacion.js
@@ -52,82 +52,96 @@ const insertplanilla = async (request, response) => {
         let resultados = [];
         let estructuras = request.body.estructuras;
         let n_idpro_proyecto = request.body.n_idpro_proyecto;
-        estructuras.forEach(async element => {
-            try {
-                var cadena = 'select id,c_mensaje, b_flag,n_flag from fn_node_insert_planilla_lprp( ' +
-                    element.TIPO + ',\'' +
-                    element.CIRCUITO + '\',\'' +
-                    element.LINEA + '\',\'' + //*
-                    element.NRO_SSEE + '\',\'' + //*
-                    element.ESTRUCTURA + '\',\'' +
-                    element.ESTRUCTURA_ANTERIOR + '\',\'' +
-                    element.ARMADO_P + '\',\'' +
-                    element.ARMADO_S + '\',\'' +
-                    element.ARMADO_RS_SE + '\',' +
-                    element.PROGRESIVA + ',' +
-                    element.VANO + ',\'' +
-                    element.T_TERRENO + '\',' +
-                    element.S_CANTIDAD + ',\'' +
-                    element.S_TIPO + '\',\'' +
-                    element.COOR_E + '\',\'' +
-                    element.COOR_N + '\',\'' +
-                    element.TIPO_CONDUCTOR + '\',' +
-                    element.CANTIDAD_CONDUCTOR + ',' +
-                    element.AISLADOR_56_3 + ',' +
-                    element.AISLADOR_POLIMERICO + ',' +
-                    element.AMOR_35 + ',' +
-                    element.AMOR_70 + ',' +
-                    element.RI_A + ',' +
-                    element.RV_A + ',' +
-                    element.RI_MT + ',' +
-                    element.RV_MT + ',' +
-                    element.RI + ',' +
-                    element.RV + ',' +
-                    element.RIY + ',' +
-                    element.RVY + ',' +
-                    element.AP_BT + ',' +
-                    element.AP_BT_ANGULO + ',' +
-                    element.AP_MT + ',' +
-                    element.AP_MT_ANGULO + ',' +
-                    element.PAT_1 + ',' +
-                    element.PAT_1S + ',' +
-                    element.PAT_1C + ',' +
-                    element.PAT_2 + ',' +
-                    element.PAT_3 + ',' +
-                    element.PAT_1CS + ',' +
-                    element.PAT_2S + ',' +
-                    element.PAT_3S + ',\'' +
-                    element.COTA + '\',\'' +
-                    element.ANGULO + '\',\'' +
-                    element.VERTICE + '\',\'' +
-                    element.TIPO_TRANS + '\',' +
-                    element.CANTIDAD_TRANS + ',' +
-                    element.FASES + ',' +
-                    element.ZONA_UTM + ',\'' +
-                    element.FUNCION_ARMADO + '\',' +
-                    element.VERSION +
-                    ','+ n_idpro_proyecto +')';
-                let queryImportacion = await pool.query(cadena);
 
-                if (queryImportacion.rowCount > 0) {
-                    element.b_flag = queryImportacion.rows[0].b_flag
-                    element.c_mensaje = queryImportacion.rows[0].c_mensaje
-                    resultados.push(element);
-                    if (!queryImportacion.rows[0].b_flag) {
-                        console.log("insertplanilla cadena", cadena);
+        let arr = request.body.estructuras;
+        let arr2 = []
+        arr.forEach(element => {
+            arr2.push( JSON.stringify(element) )
+        });
+        
+        let cadena = 'SELECT fn_node_armado_val(\'[' + arr2 + ']\', '+n_idpro_proyecto+')'
+        pool.query(cadena,(error, results) => {
+            if (error) {
+                console.log(error);
+                response.status(200).json({ estado: false, mensaje: "DB: error3!.", data: null })
+            } else {
+                estructuras.forEach(async element => {
+                    try {
+                        var cadena = 'select id,c_mensaje, b_flag,n_flag from fn_node_insert_planilla_lprp( ' +
+                            element.TIPO + ',\'' +
+                            element.CIRCUITO + '\',\'' +
+                            element.LINEA + '\',\'' + //*
+                            element.NRO_SSEE + '\',\'' + //*
+                            element.ESTRUCTURA + '\',\'' +
+                            element.ESTRUCTURA_ANTERIOR + '\',\'' +
+                            element.ARMADO_P + '\',\'' +
+                            element.ARMADO_S + '\',\'' +
+                            element.ARMADO_RS_SE + '\',' +
+                            element.PROGRESIVA + ',' +
+                            element.VANO + ',\'' +
+                            element.T_TERRENO + '\',' +
+                            element.S_CANTIDAD + ',\'' +
+                            element.S_TIPO + '\',\'' +
+                            element.COOR_E + '\',\'' +
+                            element.COOR_N + '\',\'' +
+                            element.TIPO_CONDUCTOR + '\',' +
+                            element.CANTIDAD_CONDUCTOR + ',' +
+                            element.AISLADOR_56_3 + ',' +
+                            element.AISLADOR_POLIMERICO + ',' +
+                            element.AMOR_35 + ',' +
+                            element.AMOR_70 + ',' +
+                            element.RI_A + ',' +
+                            element.RV_A + ',' +
+                            element.RI_MT + ',' +
+                            element.RV_MT + ',' +
+                            element.RI + ',' +
+                            element.RV + ',' +
+                            element.RIY + ',' +
+                            element.RVY + ',' +
+                            element.AP_BT + ',' +
+                            element.AP_BT_ANGULO + ',' +
+                            element.AP_MT + ',' +
+                            element.AP_MT_ANGULO + ',' +
+                            element.PAT_1 + ',' +
+                            element.PAT_1S + ',' +
+                            element.PAT_1C + ',' +
+                            element.PAT_2 + ',' +
+                            element.PAT_3 + ',' +
+                            element.PAT_1CS + ',' +
+                            element.PAT_2S + ',' +
+                            element.PAT_3S + ',\'' +
+                            element.COTA + '\',\'' +
+                            element.ANGULO + '\',\'' +
+                            element.VERTICE + '\',\'' +
+                            element.TIPO_TRANS + '\',' +
+                            element.CANTIDAD_TRANS + ',' +
+                            element.FASES + ',' +
+                            element.ZONA_UTM + ',\'' +
+                            element.FUNCION_ARMADO + '\',' +
+                            element.VERSION +
+                            ','+ n_idpro_proyecto +')';
+                        let queryImportacion = await pool.query(cadena);
+        
+                        if (queryImportacion.rowCount > 0) {
+                            element.b_flag = queryImportacion.rows[0].b_flag
+                            element.c_mensaje = queryImportacion.rows[0].c_mensaje
+                            resultados.push(element);
+                            if (!queryImportacion.rows[0].b_flag) {
+                                console.log("insertplanilla cadena", cadena);
+                            }
+                        }
+                    } catch (error) {
+                        element.b_flag = false;
+                        element.c_mensaje = "error:" + error;
+                        resultados.push(element);
                     }
-                }
-            } catch (error) {
-                element.b_flag = false;
-                element.c_mensaje = "error:" + error;
-                resultados.push(element);
-            }
-            i++;
-            if (estructuras.length <= i) {
-                response.status(200).json({ estado: true, mensaje: "vueltas: " + i, data: resultados });
+                    i++;
+                    if (estructuras.length <= i) {
+                        response.status(200).json({ estado: true, mensaje: "vueltas: " + i, data: resultados });
+                    }
+                });
             }
         });
-
     } else {
         response.status(200).json(obj)
     }
